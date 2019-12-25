5dd617582528f0aa2ad9c4c424637f550227f2cd
Ilya Denisov <i.denisovs@gmail.com>
2019-12-25 15:28:49 +0200

    Config files for BMP280.

---
+++ src/registers.ts
@@ -0,0 +1,17 @@
+export const id = 0xD0;
+export const reset = 0xE0;
+export const status = 0xF3;
+export const measure = 0xF4;
+export const config = 0xF5;
+
+export const press = {
+    msb: 0xF7,
+    lsb: 0xF8,
+    xlsb: 0xF9
+};
+
+export const temp = {
+    msb: 0xFA,
+    lsb: 0xFB,
+    xlsb: 0xFC
+};
